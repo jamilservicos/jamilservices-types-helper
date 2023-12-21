fa3e8d649f2e98cab660fd5c51f065d7e4f95f4e
JamilServices <jamilservicos@gmail.com>
2023-12-21 06:28:34 -0300

    Add test for module typesCheck exports and testInternalExports.

--- src/mods/typesCheck.spec.js
+++ src/mods/typesCheck.spec.js
@@ -1,1 +1,11 @@
 "use strict";
+
+const {test} = require("node:test");
+const {equal} = require("node:assert");
+
+const testExports = (typeof require("./typesCheck") === "object");
+const testInternalExports = (typeof require("./typesCheck").typesCheck === "function");
+test("module typesCheck exports test", () => {
+    equal(testExports, true);
+    equal(testInternalExports, true);
+});
