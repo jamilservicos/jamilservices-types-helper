67a332fcb0f933652282ac00e8fceecddc410c4a
JamilServices <jamilservicos@gmail.com>
2023-12-23 21:01:36 -0300

    Refactor typesCheck.js module for improved readability and performance.

--- src/mods/typesCheck.js
+++ src/mods/typesCheck.js
@@ -4,9 +4,13 @@ const {TypeException, responseError} = require("./typeException");
 const {customTypes} = require("./customTypes");
 
 /**
+ * Filters the given types to include only those that are defined in the customTypes.
+ *
+ * @private
  * @function typeFilter
- * @param {Object} types
- * @returns {Set<*>}
+ * @memberof module:TypesCheckModule
+ * @param {Object} types - The types to be filtered.
+ * @returns {Set<*>} - A Set of types that are both in the input and the customTypes.
  */
 const typeFilter = (types) => {
     const custom_types = new Set(Object.keys(customTypes));
@@ -14,20 +18,29 @@ const typeFilter = (types) => {
 };
 
 /**
+ * Evaluates if the provided data in obj matches the specified type key.
+ *
+ * @private
  * @function justOneTypeResponse
- * @param {string} key
- * @param {Object} obj
- * @returns {{error: TypeException}|boolean}
+ * @memberof module:TypesCheckModule
+ * @param {string} key - The key representing the type in customTypes to check against.
+ * @param {Object} obj - The object containing the data to be checked.
+ * @returns {{error: TypeException}|boolean} - Returns true if data matches the type, otherwise returns an error object.
  */
 const justOneTypeResponse = (key, obj) => {
     if((typeof customTypes[key] === "function") && (customTypes[key](obj.data))) return true;
     return responseError(obj);
 };
+
 /**
+ * Evaluates if the provided data in obj matches any of the specified multiple type keys.
+ *
+ * @private
  * @function multiplesTypesResponse
- * @param {Object} keys
- * @param {Object} obj
- * @returns {{error: TypeException}|boolean}
+ * @memberof module:TypesCheckModule
+ * @param {Object} keys - The keys representing the types in customTypes to check against.
+ * @param {Object} obj - The object containing the data to be checked.
+ * @returns {{error: TypeException}|boolean} - Returns true if data matches any of the types, otherwise returns an error object.
  */
 const multiplesTypesResponse = (keys, obj) => {
     const {data} = obj;
@@ -39,9 +52,17 @@ const multiplesTypesResponse = (keys, obj) => {
 };
 
 /**
+ * Checks if the provided data matches any of the specified types.
+ *
+ * This function determines if the provided data in obj matches the specified type(s) in customTypes or JavaScript primitive types.
+ * It can handle both single and multiple type checks.
+ *
  * @function typesCheck
- * @param {Object} obj
- * @returns {{error: TypeException}|boolean}
+ * @memberof module:TypesCheckModule
+ * @param {Object} obj - The object containing the data and the types to be checked.
+ * @param {any} obj.data - The data to be evaluated against the specified types.
+ * @param {Array.<string>|string} obj.types - The types to check the data against.
+ * @returns {{error: TypeException}|boolean} - Returns true if data matches any of the types, otherwise returns an error object.
  */
 const typesCheck = (obj) => {
     const {data, types} = obj;
@@ -61,7 +82,11 @@ const typesCheck = (obj) => {
 };
 
 /**
+ * Exports the typesCheck function.
+ * This function is intended for internal use within the module or package.
+ *
  * @private
+ * @module TypesCheckModule
  */
 exports = module.exports = {
     ...{typesCheck}
