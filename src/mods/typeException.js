6e49947a6436ffb5da7ef079d13ea22434ad53c9
JamilServices <jamilservicos@gmail.com>
2023-12-23 21:01:41 -0300

    Refactor TypeException class and responseError function for better error handling.

--- src/mods/typeException.js
+++ src/mods/typeException.js
@@ -1,6 +1,22 @@
 "use strict";
 
+/**
+ * Custom error class for handling type exceptions.
+ *
+ * This class extends the built-in Error class and provides a customized message
+ * for type-related errors, indicating what type was expected and what was received.
+ *
+ * @class TypeException
+ * @extends {Error}
+ */
 class TypeException extends Error {
+    /**
+     * Creates an instance of TypeException.
+     *
+     * @constructor
+     * @param {any} data - The data that caused the exception due to a type mismatch.
+     * @param {string|string[]} type - The expected type(s) that the data was supposed to adhere to.
+     */
     constructor(data, type) {
         const code = "TypeError";
         let message = `Argument type ${typeof data} is not assignable to parameter type ${type}`;
@@ -10,16 +26,26 @@ class TypeException extends Error {
         this.name = this.code = code;
         this.message = response;
     }
-
+    /**
+     * Converts the error object to its string representation.
+     *
+     * @returns {string} - The string representation of the error.
+     */
     toString() {
         return this.message;
     }
 }
 
 /**
+ * Generates a TypeException error object for a given data and expected type(s).
+ *
+ * This function is a helper to create a standardized error object when there is a type mismatch.
+ *
  * @function responseError
- * @param {Object} obj
- * @returns {{error: TypeException}}
+ * @param {Object} obj - The object containing the data and the expected type(s).
+ * @param {any} obj.data - The data that caused the type exception.
+ * @param {string|string[]} obj.types - The expected type(s) that the data was supposed to adhere to.
+ * @returns {{error: TypeException}} - An object containing the TypeException error.
  */
 const responseError = (obj) => {
     const {data, types} = obj;
@@ -27,6 +53,9 @@ const responseError = (obj) => {
 };
 
 /**
+ * Exports the TypeException class and responseError function.
+ * These are primarily used for handling and reporting type-related errors.
+ *
  * @private
  */
 exports = module.exports = {
